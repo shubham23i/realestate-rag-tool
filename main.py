ada2e4f16cdc0568e0041852b1b103457e277591
shubham23i <indsjos@gmail.com>
2026-01-06 13:28:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# @Author: Dhaval Patel Copyrights Codebasics Inc. and LearnerX Pvt Ltd.
+
 
 import streamlit as st
 from rag import process_urls, generate_answer
@@ -10,8 +10,8 @@ url2 = st.sidebar.text_input("URL 2")
 url3 = st.sidebar.text_input("URL 3")
 
 placeholder = st.empty()
-
 process_url_button = st.sidebar.button("Process URLs")
+
 if process_url_button:
     urls = [url for url in (url1, url2, url3) if url!='']
     if len(urls) == 0:
@@ -26,7 +26,6 @@ if query:
         answer, sources = generate_answer(query)
         st.header("Answer:")
         st.write(answer)
-
         if sources:
             st.subheader("Sources:")
             for source in sources.split("\n"):
